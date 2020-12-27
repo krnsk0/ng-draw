441adf03ba2cfb1d33d364d71a4146f5a9ddf25f
krnsk0 <jkurinsky@gmail.com>
2020-12-27 17:20:50 -0500

    better prettier config

--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tools',
   templateUrl: './tools.component.html',
-  styleUrls: ['./tools.component.css']
+  styleUrls: ['./tools.component.css'],
 })
 export class ToolsComponent implements OnInit {
   tools: Array<string> = ['Circle', 'Rectangle'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  clickHandler(toolName: string): void {
+    console.log(`${toolName} clicked`);
   }
-
 }
